38d5d5a637e29315e3fc6f97416d8e4f671102da
benrabah mohamed <benrabahmohamed530@gmail.com>
2022-10-12 22:06:48 +0100

    course information step5

--- part1/courseinformation/src/App.jsx
+++ part1/courseinformation/src/App.jsx
@@ -1,25 +1,27 @@
 const App = () => {
-    const course = 'Half Stack application development'
-    const parts = [
-        {
-            name: 'Fundamentals of React',
-            exercise: 10
-        },
-        {
-            name: 'Using props to pass data',
-            exercise: 7
-        },
-        {
-            name: 'State of a component',
-            exercise: 14
-        }
-    ]
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercise: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercise: 7
+            },
+            {
+                name: 'State of a component',
+                exercise: 14
+            }
+        ]
+    }
     
     return (  
         <div>
-            <Header course={course} />
-            <Content parts={parts} />
-            <Total parts={parts} />
+            <Header course={course.name} />
+            <Content parts={course.parts} />
+            <Total parts={course.parts} />
         </div>
     );
 }
