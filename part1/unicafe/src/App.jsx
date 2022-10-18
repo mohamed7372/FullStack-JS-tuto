6ceee84b357b099e8ca0fc543ed7e7ff708a762c
benrabah mohamed <benrabahmohamed530@gmail.com>
2022-10-18 19:32:22 +0100

    unicafe step 1

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -9,15 +9,19 @@ const App = () => {
   return (
     <div>
       <h1>Give feedback</h1>
-      <button>good</button>
-      <button>neutral</button>
-      <button>bad</button>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
       <h1>Statistics</h1>
-      <p>good 6</p>
-      <p>netral 2</p>
-      <p>bad 1</p>
+      <Stat title={'good'} stat={good}/>
+      <Stat title={'neutral'} stat={neutral}/>
+      <Stat title={'bad'} stat={bad}/>
     </div>
   )
+}
+
+const Stat = ({title, stat}) => {
+  return(<p>{title} {stat}</p>)
 }
 
 export default App
