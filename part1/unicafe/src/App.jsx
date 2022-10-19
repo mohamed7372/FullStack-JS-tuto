424834fe6037cddf640acafaac2d6eae21568389
benrabah mohamed <benrabahmohamed530@gmail.com>
2022-10-19 08:43:59 +0100

    unicafe step 4

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -15,12 +15,20 @@ const App = () => {
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
       <h1>Statistics</h1>
-      <Statistics title={'good'} stat={good}/>
-      <Statistics title={'neutral'} stat={neutral}/>
-      <Statistics title={'bad'} stat={bad}/>
-      <Statistics title={'all'} stat={good + neutral + bad}/>
-      <Statistics title={'average'} stat={(good + bad*-1) / (good + neutral + bad)}/>
-      <Statistics title={'positive'} stat={(good * 100) / (good + neutral + bad) + ' %'} />
+      {good + bad + neutral == 0 ?
+        (<p>No feedback given</p>)
+        :
+        (
+          <div>
+            <Statistics title={'good'} stat={good}/>
+            <Statistics title={'neutral'} stat={neutral}/>
+            <Statistics title={'bad'} stat={bad}/>
+            <Statistics title={'all'} stat={good + neutral + bad}/>
+            <Statistics title={'average'} stat={(good + bad*-1) / (good + neutral + bad)}/>
+            <Statistics title={'positive'} stat={(good * 100) / (good + neutral + bad) + ' %'} />
+          </div>    
+        )
+      }
     </div>
   )
 }
