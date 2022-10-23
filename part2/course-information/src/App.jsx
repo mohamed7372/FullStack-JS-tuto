21137eb5435f1d038aed9df2e6f61babf7b0b185
benrabah mohamed <benrabahmohamed530@gmail.com>
2022-10-23 17:10:06 +0100

    course information step 7

--- part2/course-information/src/App.jsx
+++ part2/course-information/src/App.jsx
@@ -16,6 +16,11 @@ const App = () => {
             name: 'State of a component',
             exercises: 14,
             id: 3
+        },
+        {
+            name: 'Redux',
+            exercises: 11,
+            id: 4
         }
         ]
     }
@@ -23,7 +28,7 @@ const App = () => {
     return <Course course={course} />
 }
 
-const Course = ({course}) => {
+const Course = ({ course }) => {
     return (  
         <div>
             <Header course={course.name} />
@@ -39,14 +44,17 @@ const Header = ({course}) => {
 }
 
 const Content = ({ parts }) => {
+    const total = parts.reduce((currValue, element) => currValue + element['exercises'], 0);
+    
     return (
         <>
             {parts.map((element, index) => {
                 return (
-                    <Part key={index} name={element['name']} exercise={ element['exercises']} />
+                    <Part key={element['id']} name={element['name']} exercise={ element['exercises']} />
                 );
             })
             }
+            <p><b>total of {total} exercises</b></p>
         </>
     )
 }
