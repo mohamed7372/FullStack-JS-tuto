1a170c2d2c05399bc24b0c38bf22de94715c4fe9
benrabah mohamed <benrabahmohamed530@gmail.com>
2022-10-27 09:52:07 +0100

    the phonebook step 4

--- part2/the-phonebook/src/App.jsx
+++ part2/the-phonebook/src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from 'react'
 
+import Number from './components/Number'
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
+  
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  
+  const [search, setSearch] = useState('')
 
   const handleNewPhone = event => {
     event.preventDefault();
@@ -26,9 +34,19 @@ const App = () => {
     setNewNumber('');
   }
 
+  const allPerson = persons.map(person => <Number key={person.id} person={person}/>)
+
+  const searchPerson = persons
+    .filter(person => person.name.toLowerCase().includes(search.toLowerCase()))
+    .map(person => <Number key={person.id} person={person}/>)
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with: <input value={search} onChange={(event) => setSearch(event.target.value)}/>
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={handleNewPhone}>
         <div>
           name: <input value={newName} onChange={(event) => setNewName(event.target.value)}/>
@@ -41,17 +59,9 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => <Number key={person.name} person={person}/>)}
+      {search === '' ? (allPerson):(searchPerson)}
     </div>
   )
 }
 
-export default App
-
-const Number = ({ person }) => {
-  return (
-    <>
-      {person.name} { person.number}<br />
-    </>
-  );
-}
+export default App;
---
+++ part2/the-phonebook/src/components/Number.jsx
@@ -0,0 +1,10 @@
+const Number = ({ person }) => {
+  return (
+    <>
+      {person.name} { person.number}<br />
+    </>
+  );
+}
+ 
+export default Number;
+
