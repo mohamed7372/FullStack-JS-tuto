faf3b09c4a5d3752b26a6780f75c1a6096b1f86a
benrabah mohamed <benrabahmohamed530@gmail.com>
2022-12-27 10:07:51 +0100

    phonebook backend step 8

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -4,7 +4,19 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan((tokens, req, res) => {
+    const body = tokens.method(req, res) === 'POST'
+        ? JSON.stringify(req.body)
+        : '';
+    return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, 'content-length'), '-',
+        tokens['response-time'](req, res), 'ms',
+        body    
+    ].join(' ')
+}))
 
 let persons = [
     { 
