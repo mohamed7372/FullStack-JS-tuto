8b2e76e5d5e97bec21b1cec87e7d60b54e55e72e
benrabah mohamed <benrabahmohamed530@gmail.com>
2023-03-23 10:47:19 +0100

    phonebook database step 6

--- part3/phonebook-database/index.js
+++ part3/phonebook-database/index.js
@@ -1,10 +1,44 @@
+const express = require('express')
+const app = express()
+const cors =require('cors')
 require('dotenv').config()
 
-const express = require('express')
 const Person = require('./models/person')
 
-const app = express()
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+}
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message);
+
+    if (error.name === 'CastError')
+        response.status(400).send({ error: 'malformated id' })
+    
+    next(error)
+}
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// app.use(cors)
 app.use(express.json())
+app.use(requestLogger)
+app.use(express.static('build'))
+
+app.get('/info', (request, response) => {
+    Person.count({})
+        .then(numOfDocs => {
+            response.send(
+                `<p>Phonebook has info for ${numOfDocs} people</p>${Date()}<p></p>`
+                )
+        })
+})
 
 app.get('/api/persons', (request, response) => {
     Person.find({})
@@ -29,40 +63,37 @@ app.delete('/api/persons/:id', (request, response, next) => {
         .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
-
     const newPerson = new Person({
         name: body.name,
         number: body.number
     })
 
-    newPerson.save().then(savedPerson => {
-        response.json(savedPerson)
-    })
-})
-
-app.get('/info', (request, response) => {
-    Person.count({})
-        .then(numOfDocs => {
-            response.send(
-                `<p>Phonebook has info for ${numOfDocs} people</p>${Date()}<p></p>`
-                )
+    newPerson.save()
+        .then(savedPerson => {
+            response.json(savedPerson)
         })
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
 
-const errorHandler = (error, request, response, next) => {
-    console.error(error.message);
+    const newPerson = {
+        name: body.name,
+        number: body.number
+    }
 
-    if (error.name === 'CastError')
-        response.status(400).send({ error: 'malformated id' })
-    
-    next(error)
-}
+    Person
+        .findByIdAndUpdate(request.params.id, newPerson, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
 
+app.use(unknownEndpoint)
 app.use(errorHandler)
-
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
