fa01cd00f3683bfe3f0cbce680ff27d28e41628b
benrabah mohamed <benrabahmohamed530@gmail.com>
2023-03-24 10:28:44 +0100

    phonebook database step 8

--- part3/phonebook-database/index.js
+++ part3/phonebook-database/index.js
@@ -17,7 +17,7 @@ const errorHandler = (error, request, response, next) => {
     console.error(error.message);
 
     if (error.name === 'CastError')
-        response.status(400).send({ error: 'malformated id' })
+        return response.status(400).send({ error: 'malformated id' })
     else if (error.name === 'ValidationError') 
         return response.status(400).json({ error: error.message })
     
--- part3/phonebook-database/models/person.js
+++ part3/phonebook-database/models/person.js
@@ -18,7 +18,16 @@ const personSchema = new mongoose.Schema({
         type: String,
         minLength: 3
     },
-    number: String,
+    number: {
+        type: String,
+        minLength: 8,
+        validate: {
+            validator: function(v) {
+                return /[\d{2}|\d{3}]-\d+/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        },
+    },
 })
 
 // remove id and version 
